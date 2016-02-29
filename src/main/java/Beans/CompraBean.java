02fa73802f88eeecf146c3d307e56b20a336bf89
Nabil <nabilchamasi@gmail.com>
2016-02-29 12:11:02 -0300

    /compras rest ver2

--- src/main/java/Beans/CompraBean.java
+++ src/main/java/Beans/CompraBean.java
@@ -1,7 +1,41 @@
 package Beans;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 /**
  * Created by nabil on 29/02/16.
  */
 public class CompraBean {
+
+    public static HashMap<Integer, CompraBean> compras= new HashMap<Integer, CompraBean>();
+
+    private Integer id;
+    private static Integer idCounter=0;
+    private ProveedorBean proveedor;
+    private ArrayList<ProductoBean> productos;
+
+    public CompraBean() {
+        this.id = idCounter++;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public ProveedorBean getProveedor() {
+        return proveedor;
+    }
+
+    public void setProveedor(ProveedorBean proveedor) {
+        this.proveedor = proveedor;
+    }
+
+    public ArrayList<ProductoBean> getProductos() {
+        return productos;
+    }
+
+    public void setProductos(ArrayList<ProductoBean> productos) {
+        this.productos = productos;
+    }
 }
--- src/main/java/REST/Compra.java
+++ src/main/java/REST/Compra.java
@@ -1,7 +1,54 @@
 package REST;
 
+import Beans.CompraBean;
+import Beans.ProductoBean;
+import Beans.ProveedorBean;
+
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Response;
+import java.util.ArrayList;
+import java.util.HashMap;
+
 /**
  * Created by nabil on 29/02/16.
  */
+
+@Path("/compras")
 public class Compra {
+
+    private static HashMap<Integer, CompraBean> compras = CompraBean.compras;
+    private static HashMap<Integer, ProveedorBean> proveedores = ProveedorBean.proveedores;
+    private static HashMap<Integer, ProductoBean> productos = ProductoBean.productos;
+
+    @POST
+    @Produces("application/json")
+    public Response crearCompra(@QueryParam("proveedorId") Integer proveedorId,
+                                @QueryParam("productosId") ArrayList<Integer> productosId){
+
+        if(!proveedores.containsKey(proveedorId)){
+            return Response.status(200).entity("Proveedor no existe.").build();
+        }else{
+            ProveedorBean proveedor = proveedores.get(proveedorId);
+            ArrayList<ProductoBean> productosCompra = new ArrayList<ProductoBean>();
+
+            for (int i = 0; i < productosId.size(); i++) {
+                if(productos.containsKey(productosId.get(i))){
+                    ProductoBean producto = productos.get(productosId.get(i));
+                    productosCompra.add(producto);
+                }else{
+                    return Response.status(200).entity("Producto no existe.").build();
+                }
+            }
+
+            CompraBean compra = new CompraBean();
+            compra.setProveedor(proveedor);
+            compra.setProductos(productosCompra);
+            compras.put(compra.getId(), compra);
+            return Response.status(200).entity(compra).build();
+        }
+
+    }
 }
