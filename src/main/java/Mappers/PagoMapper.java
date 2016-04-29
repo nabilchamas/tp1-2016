07248ff7449136bc326913a427f991d1d8b6c404
Santiago Ortiz <sanmiorto@gmail.com>
2016-04-29 19:19:20 -0400

    Pago Mapper basico

--- src/main/java/Mappers/PagoMapper.java
+++ src/main/java/Mappers/PagoMapper.java
@@ -1,7 +1,17 @@
 package Mappers;
 
+import JPA.PagoEntity;
+
+import java.util.List;
+
 /**
  * Created by sortiz on 4/29/16.
  */
 public interface PagoMapper {
+
+    void insertPago(PagoEntity pago);
+    PagoEntity getPagoById(Integer pagoId);
+    List<PagoEntity> getAllPagos();
+    void updatePago(PagoEntity pago);
+    void deletePago(Integer pagoId);
 }
